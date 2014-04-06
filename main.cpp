18edd2d580bf547614ed1850cdc0effd88a04009
Michael <meswaddle@netspace.net.au>
2014-04-06 11:37:17 +0800

    gnu gpl

--- main.cpp
+++ main.cpp
@@ -1,4 +1,22 @@
 
+/*
+Geodesic solver for curves in SU(2^n)
+Copyright (C) 2014  Michael Swaddle
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 #include <cmath>
 #include <iostream>
@@ -7,20 +25,7 @@ using namespace std;
 
 /*
 
-	 1) Enumerate k with a guesses for the boundary say k(0) = I,  k(1) = InverseMatExp(U)
-
-	 2) Shoot from the left and enumerate x'(i) = k(i) x(i) with the guess for k(0), x(0) = I find xLeft
-
-	 3) Now shoot from the right and enumerate x'(i) = k(i) x(i) with the values for k(1), x(1) = U 
-	  	find xRight
 	
-	 4) take the matrix norms of xRight and xLeft 
-
-	 5) Evaluate xLeft - xRight = M 
-
-	 6) Take k(0) =  I - InverseMatExp(M)
-	
-
 
 
 
